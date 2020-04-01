3365d27fef8f499753cafaf228c5d6e3dc02cc8e
Phi Huynh <phuynh7577@gmail.com>
2020-04-01 19:10:15 -0400

    installed axios and updated App.js

---
+++ asisme_app/src/components/States.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+
+class States extends React.Component {
+    render() {
+        return(
+            <div className="states">
+
+            </div>
+        )
+    }
+}
+
+
+export default App;

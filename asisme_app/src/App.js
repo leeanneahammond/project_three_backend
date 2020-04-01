ab2ad1df3f96d131f46e408eafff07d66af54e68
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-04-01 19:10:38 -0400

    added basic react-router

--- asisme_app/src/App.js
+++ asisme_app/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 
 
+function Index(props) {
+  return <h2>Hello, {props.username}</h2>;
+}
+
+function About() {
+  return (
+    <>
+      <h2>About</h2>
+      <p>
+        ljksflkf skjlafsdkjlfasdjklfas djklfads kjlfadskjfads jkllafksjdklfj
+        adslkjfads{" "}
+      </p>
+    </>
+  );
+}
 
 class App extends React.Component {
   state = {
@@ -11,22 +27,38 @@ class App extends React.Component {
 
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                  <Link to="/">Home</Link>
+              </li>
+              <li>
+              <Link to="/aboutus/">About</Link>
+            </li>
+            </ul>
+          </nav>
+          <div>
+          <Route path="/" exact component={Index} />
+          <Route path="/aboutus/" exact component={About} />
+          </div>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </BrowserRouter>
     )
   }
 }

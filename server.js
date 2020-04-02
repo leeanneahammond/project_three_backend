254eb386295fedd067460fb9fb4fe7551dccad0f
leeanneahammond <56902098+leeanneahammond@users.noreply.github.com>
2020-04-01 21:50:12 -0400

    Deleted mixup

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const state = express.Router();
 const PORT = 4000;
+
 // middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/state, stateRoutes')
 
-<<<<<<< HEAD
+
 mongoose.connect('mongodb://127.0.0.1:27017/state', { useNewUrlParser: true });
-=======
 mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
->>>>>>> 0c8973f3433594a663656c1dcea20ff255e0f5a5
+
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
@@ -25,10 +25,9 @@ connection.once('open', function() {
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
-<<<<<<< HEAD
 });
 
 
-=======
+
 });
->>>>>>> 0c8973f3433594a663656c1dcea20ff255e0f5a5
+

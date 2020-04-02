ae6fa95bf336a1bf7de9523255e25be118cbbbdc
Leeanne Hammond <leeannehammond@yahoo.com>
2020-04-02 14:23:54 -0400

    basic outline get/post

--- server.js
+++ server.js
@@ -3,32 +3,33 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const state = express.Router();
 const PORT = 4000;
-// middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/state, stateRoutes')
-
-<<<<<<< HEAD
-mongoose.connect('mongodb://127.0.0.1:27017/state', { useNewUrlParser: true });
-=======
 mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
->>>>>>> 0c8973f3433594a663656c1dcea20ff255e0f5a5
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
-})
+});
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+  });
+
+// GET route
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+  });
 
 
+
+
+// POST route
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+  })
 
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
-<<<<<<< HEAD
 });
-
-
-=======
-});
->>>>>>> 0c8973f3433594a663656c1dcea20ff255e0f5a5

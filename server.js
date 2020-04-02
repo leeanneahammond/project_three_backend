913f9be2edfeba3473763c9f182b28e806c74a1d
leeanneahammond <56902098+leeanneahammond@users.noreply.github.com>
2020-04-01 20:22:12 -0400

    test2

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const state = express.Router();
 const PORT = 4000;
+// middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/state, stateRoutes')
+
 mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
